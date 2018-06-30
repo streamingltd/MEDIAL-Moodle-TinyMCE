12be633135ae7590de69e96a394a56dea01bccfc
Tim Williams <tmw@autotrain.org>
2018-06-30 13:06:29 +0100

    Release version 2018090708-fix

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2018090707;
+$plugin->version   = 2018090708;
 // Required Moodle version.
 $plugin->requires  = 2016052300;
 // Full name of the plugin (used for diagnostics).
@@ -35,5 +35,5 @@ $plugin->component = 'tinymce_helixmedia';
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->dependencies = array(
-    'mod_helixmedia' => '2018090707'
+    'mod_helixmedia' => '2018090708'
 );
